5cae2ae3b2cd2b512c1737690256b889be4840d1
jerinzech <jerinzjames@gmail.com>
2024-01-07 12:55:57 +0530

    fixed bio components

--- src/components/body_components/Bio.jsx
+++ src/components/body_components/Bio.jsx
@@ -1,34 +1,41 @@
 import React from 'react'
 import FPStar from '../../assets/four-point-star.png' ;
+import BlrMap from '../../assets/blr-map.png' ;
+import GithubLogo from '../../assets/github.png' ;
+import TwitterLogo from '../../assets/twitter.png' ;
+import LinkedInLogo from '../../assets/linkedin.png' ;
+import InstagramLogo from '../../assets/instagram.png' ;
 
 export default function Bio() {
   return (
     <div className='bio-block'>
       <div className="bio-name">
         <p className='bio-tag'>Name :</p>
-        <p>Jerin James</p>
+        <p className='bio-value'>Jerin James</p>
       </div>
       <div className="bio-location">
         <div className="bio-location-text">
           <p className='bio-tag'>Based in :</p>
-          <p>Bengaluru, India</p>
+          <p className='bio-value'>Bengaluru, India</p>
         </div>
-        <div className="bio-location-map"></div>
+        <div className="bio-location-map">
+          <img className='bio-blr-map' src={BlrMap} alt="Map of Bengaluru" />
+        </div>
         
 
       </div>
       <div className="bio-social">
         <div className="github">
-          <img src={FPStar} alt="Four Point Star" />
+          <img src={GithubLogo} alt="Four Point Star" />
         </div>
         <div className="linkedIn">
-          <img src={FPStar} alt="Four Point Star" />
+          <img src={LinkedInLogo} alt="Four Point Star" />
           </div>
         <div className="twitter">
-          <img src={FPStar} alt="Four Point Star" />
+          <img src={TwitterLogo} alt="Four Point Star" />
           </div>
         <div className="instagram">
-          <img src={FPStar} alt="Four Point Star" />
+          <img src={InstagramLogo} alt="Four Point Star" />
           </div>     
       </div>
     </div>
