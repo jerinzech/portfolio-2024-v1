3d7492192f99a73669cf73544b8e8a0518cd07d5
jerinzech <jerinzjames@gmail.com>
2024-01-07 13:28:35 +0530

    populated resources area

--- src/components/body_components/Resources.jsx
+++ src/components/body_components/Resources.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function Resources() {
   return (
-    <div className='resources-block'>Resources</div>
+    <div className='resources-block'>
+
+      <div className="tbr-block">
+        <p>/tbr</p>
+      </div>
+      <div className="about-me-block">
+        <p>/about-me</p>
+      </div>
+      <div className="togglebutton">
+        <p>
+          ToggleButton
+        </p>
+      </div>
+      <div className="contact-me-block">
+        <p>
+          contact me
+        </p>
+      </div>
+    </div>
   )
 }
