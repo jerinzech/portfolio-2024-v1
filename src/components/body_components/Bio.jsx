57b611a03d9ab1ee89d2c3d0b1131e163d9b9a92
jerinzech <jerinzjames@gmail.com>
2024-01-07 10:39:03 +0530

    fixed border radius

--- src/components/body_components/Bio.jsx
+++ src/components/body_components/Bio.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import FPStar from '../../assets/four-point-star.png' ;
 
 export default function Bio() {
   return (
-    <div className='bio-block'>Bio</div>
+    <div className='bio-block'>
+      <div className="bio-name">
+        <p className='bio-tag'>Name :</p>
+        <p>Jerin James</p>
+      </div>
+      <div className="bio-location">
+        <div className="bio-location-text">
+          <p className='bio-tag'>Based in :</p>
+          <p>Bengaluru, India</p>
+        </div>
+        <div className="bio-location-map"></div>
+        
+
+      </div>
+      <div className="bio-social">
+        <div className="github">
+          <img src={FPStar} alt="Four Point Star" />
+        </div>
+        <div className="linkedIn">
+          <img src={FPStar} alt="Four Point Star" />
+          </div>
+        <div className="twitter">
+          <img src={FPStar} alt="Four Point Star" />
+          </div>
+        <div className="instagram">
+          <img src={FPStar} alt="Four Point Star" />
+          </div>     
+      </div>
+    </div>
   )
 }
