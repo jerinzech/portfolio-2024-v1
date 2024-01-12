96592da0c5f6434666b76f455102c753ad81f7b5
jerinzech <jerinzjames@gmail.com>
2024-01-12 13:34:59 +0530

    fixed background

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import './HomePage.css'
-import Header from './components/Header'
-import Body from './components/Body'
-import Footer from './components/Footer'
-
+import React from "react";
+import "./HomePage.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 export default function HomePage() {
   return (
-    <div className='main-view'>
-      <Header />
-      <Body />
-      <Footer />
+    <div className="page">
+      <div className="main-view">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
