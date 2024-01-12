1f305a5af2d289a3c539bea8c7c91fbeb159a95e
jerinzech <jerinzjames@gmail.com>
2024-01-12 12:43:56 +0530

    fixing homepage - first try

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import HomePage from './HomePage'
-import LoadingPage from './LoadingPage'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomePage from "./HomePage";
+import LoadingPage from "./LoadingPage";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <HomePage/>/ */}
-
-    <LoadingPage />
-  </React.StrictMode>,
-)
+    <HomePage />
+    {/* <LoadingPage /> */}
+  </React.StrictMode>
+);
